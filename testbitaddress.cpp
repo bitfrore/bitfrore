54c3d2cdf83bf7d7d7d97a2f5ff000bbfafe77be
Steven Pearson <steven.pearson.78@googlemail.com>
2013-06-06 13:34:14 +0200

    Generate test addreses from a known seed

--- testbitaddress.cpp
+++ testbitaddress.cpp
@@ -34,3 +34,35 @@ void testbitaddress(){
   serialprint(address);
   serialprint("\r\n");
 }
+
+void testbitaddress_s(){
+  serialprint("testbitaddressseed\r\n");
+
+  uint8_t PRIVATE[]={0x18,0xE1,0x4A,0x7B,0x6A,0x30,0x7F,0x42,0x6A,0x94,0xF8,0x11,0x47,0x01,0xE7,0xC8,0xE7,0x74,0xE7,0xF9,0xA4,0x7E,0x2C,0x20,0x35,0xDB,0x29,0xA2,0x06,0x32,0x17,0x25};
+
+  for(;;){
+    WORD xbuf[BIGNATBUFSIZE(32)];
+    WORD ybuf[BIGNATBUFSIZE(32)];
+    struct bigint pubx(xbuf,sizeof(xbuf)/__S);
+    struct bigint puby(ybuf,sizeof(ybuf)/__S);
+
+    bitaddress::generatePublicKey(PRIVATE,pubx,puby);
+
+    char wif[60];
+    bitaddress::generatePrivateWIF(PRIVATE,wif,sizeof(wif));
+    serialprint(wif);
+    serialprint("\n");
+
+    char address[36];
+    bitaddress::generateAddress(pubx,puby,address,sizeof(address));
+    serialprint(address);
+    serialprint("\n");
+
+    //HASH the last private key
+    struct SHA256_CTX ctx;
+    sha256_init(&ctx);
+    sha256_update(&ctx,PRIVATE,32);
+    
+    sha256_final(&ctx,PRIVATE);
+  }
+}
